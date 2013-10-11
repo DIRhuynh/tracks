605ae05d7d675678618b28841cc9355526c0c8ee
John Huynh <dirhuynh@gmail.com>
2013-10-11 10:50:22 -0400

    Renamed current user to user.

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -1,9 +1,9 @@
 class TagCloud
-  attr_reader :current_user, :tags_for_cloud, :tags_min, :tags_divisor,
+  attr_reader :user, :tags_for_cloud, :tags_min, :tags_divisor,
     :tags_for_cloud_90days, :tags_min_90days, :tags_divisor_90days
 
-  def initialize(current_user, cut_off_3months)
-    @current_user    = current_user
+  def initialize(user, cut_off_3months)
+    @user    = user
     @cut_off_3months = cut_off_3months
   end
 
@@ -19,7 +19,7 @@ class TagCloud
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
     query << " AND taggings.taggable_id = todos.id"
-    query << " AND todos.user_id="+current_user.id.to_s+" "
+    query << " AND todos.user_id="+user.id.to_s+" "
     query << " AND taggings.taggable_type='Todo' "
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
@@ -47,7 +47,7 @@ class TagCloud
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
     @tags_for_cloud_90days = Tag.find_by_sql(
-      [query, current_user.id, @cut_off_3months, @cut_off_3months]
+      [query, user.id, @cut_off_3months, @cut_off_3months]
     ).sort_by { |tag| tag.name.downcase }
 
     max_90days, @tags_min_90days = 0, 0
