4dae1214e81841bba925fd8f572e23a43630564c
John Huynh <dirhuynh@gmail.com>
2013-10-11 13:28:30 -0400

    Added tags method to supplant @tags reader.

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -2,25 +2,28 @@ class TagCloud
 # tag cloud code inspired by this article
 #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
-  attr_reader :user, :tags, :tags_min, :tags_divisor
+  attr_reader :user, :tags_min, :tags_divisor
 
   def initialize(user, cut_off = nil)
     @user    = user
     @cut_off = cut_off
   end
 
-  # TODO: parameterize limit
-  def compute
-    levels=10
-
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-      params += [@cut_off, @cutoff]
+  def tags
+    unless @tags
+      params = [sql(@cut_off), user.id]
+      if @cut_off
+        params += [@cut_off, @cutoff]
+      end
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
     end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
+    @tags
+  end
+
+  def compute
     max, @tags_min = 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @tags_min = [t.count.to_i, @tags_min].min
     }
@@ -29,6 +32,11 @@ class TagCloud
   end
 
   private
+
+  # TODO: parameterize limit
+  def levels
+    10
+  end
 
   def sql(cut_off = nil)
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
