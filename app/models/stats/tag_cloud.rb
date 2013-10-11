42f2699fb00f5d75ab8c4069132771a8d1c51991
John Huynh <dirhuynh@gmail.com>
2013-10-11 11:22:52 -0400

    Extracted sql for 90days to private method.

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -3,7 +3,7 @@ class TagCloud
 #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
   attr_reader :user, :tags, :tags_min, :tags_divisor,
-    :tags_for_90days, :tags_min_90days, :tags_divisor_90days
+    :tags_90days, :tags_min_90days, :tags_divisor_90days
 
   def initialize(user, cut_off)
     @user    = user
@@ -33,6 +33,22 @@ class TagCloud
 
     @tags_divisor = ((max - @tags_min) / levels) + 1
 
+    @tags_90days = Tag.find_by_sql(
+      [sql_90days, user.id, @cut_off, @cut_off]
+    ).sort_by { |tag| tag.name.downcase }
+
+    max_90days, @tags_min_90days = 0, 0
+    @tags_90days.each { |t|
+      max_90days = [t.count.to_i, max_90days].max
+      @tags_min_90days = [t.count.to_i, @tags_min_90days].min
+    }
+
+    @tags_divisor_90days = ((max_90days - @tags_min_90days) / levels) + 1
+  end
+
+  private
+
+  def sql_90days
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
@@ -44,16 +60,5 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
-    @tags_for_90days = Tag.find_by_sql(
-      [query, user.id, @cut_off, @cut_off]
-    ).sort_by { |tag| tag.name.downcase }
-
-    max_90days, @tags_min_90days = 0, 0
-    @tags_for_90days.each { |t|
-      max_90days = [t.count.to_i, max_90days].max
-      @tags_min_90days = [t.count.to_i, @tags_min_90days].min
-    }
-
-    @tags_divisor_90days = ((max_90days - @tags_min_90days) / levels) + 1
   end
 end
